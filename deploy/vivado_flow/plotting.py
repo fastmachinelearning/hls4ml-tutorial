7d584dd2afe3b866412df737f32d78546b238cc8
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2021-02-04 10:57:43 -0500

    [deploy/vivado_flow] Update Vivado flow

---
+++ deploy/vivado_flow/plotting.py
@@ -0,0 +1,113 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import itertools
+
+# confusion matrix code from Maurizio
+# /eos/user/m/mpierini/DeepLearning/ML4FPGA/jupyter/HbbTagger_Conv1D.ipynb
+def plot_confusion_matrix(cm, classes,
+                          normalize=True,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    #plt.title(title)
+    cbar = plt.colorbar()
+    plt.clim(0,1)
+    cbar.set_label(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    #plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def plotRoc(fpr, tpr, auc, labels, linestyle, legend=True):
+    for i, label in enumerate(labels):
+        plt.plot(tpr[label],fpr[label],label='%s tagger, AUC = %.1f%%'%(label.replace('j_',''),auc[label]*100.),linestyle=linestyle)
+    plt.semilogy()
+    plt.xlabel("Signal Efficiency")
+    plt.ylabel("Background Efficiency")
+    plt.ylim(0.001,1)
+    plt.grid(True)
+    if legend: plt.legend(loc='upper left')
+    plt.figtext(0.25, 0.90,'hls4ml',fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+
+def rocData(y, predict_test, labels):
+
+    df = pd.DataFrame()
+
+    fpr = {}
+    tpr = {}
+    auc1 = {}
+
+    for i, label in enumerate(labels):
+        df[label] = y[:,i]
+        df[label + '_pred'] = predict_test[:,i]
+
+        fpr[label], tpr[label], threshold = roc_curve(df[label],df[label+'_pred'])
+
+        auc1[label] = auc(fpr[label], tpr[label])
+    return fpr, tpr, auc1
+
+def makeRoc(y, predict_test, labels, linestyle='-', legend=True):
+
+    if 'j_index' in labels: labels.remove('j_index')
+
+    fpr, tpr, auc1 = rocData(y, predict_test, labels)
+    plotRoc(fpr, tpr, auc1, labels, linestyle, legend=legend)
+    return predict_test
+
+def print_dict(d, indent=0):
+    align=20
+    for key, value in d.items():
+        print('  ' * indent + str(key), end='')
+        if isinstance(value, dict):
+            print()
+            print_dict(value, indent+1)
+        else:
+            print(':' + ' ' * (20 - len(key) - 2 * indent) + str(value))
+
+def plotMultiClassRoc(y_test, y_keras, labels, linestyle='-', legend=True, logscale_fpr=False, logscale_tpr=False, logo=False, title=''):
+    from sklearn.metrics import roc_curve, auc
+    tpr = dict()
+    fpr = dict()
+    roc_auc = dict()
+
+    for i, label in enumerate(labels):
+        fpr[i], tpr[i], threshold = roc_curve(y_test[:, i], y_keras[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        plt.plot(fpr[i], tpr[i], label='{0} tagger, AUC = {1:0.3f}%'.format(label, roc_auc[i]*100), linestyle=linestyle)
+
+    if legend: plt.legend(loc="lower right")
+    if logscale_tpr: plt.semilogy()
+    if logscale_fpr: plt.semilogx()
+    if logo:
+        plt.figtext(0.25, 0.90, 'hls4ml',fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+
+    if (not logscale_tpr) and (not logscale_fpr):
+        plt.plot([0, 1], [0, 1], 'k--', lw=1)
+    plt.xlim([0.000005, 1.0])
+    plt.ylim([0.000005, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(title)
+
+    plt.grid(True)
+    #_ = plt.show()
---
+++ deploy/vivado_flow/sdk/main.c
@@ -0,0 +1,323 @@
+/**
+ *
+ * Set Heap Size in ldscript.ld to 0x1000000 (16MB)
+ *
+ */
+
+#include "xmyproject.h"  /* accelerator */
+#include "stdio.h"       /* printf */
+#include "unistd.h"      /* sleep */
+#include "stdlib.h"
+#include "malloc.h"
+#include "xil_io.h"      /* peripheral read/write wrappers */
+#include "xtime_l.h"     /* to measure performance of the system */
+#include "platform.h"    /* platform init/cleanup functions */
+#include "xil_cache.h"   /* enable/disable caches etc */
+#include "xil_printf.h"  /* UART debug print functions */
+#include "xparameters.h" /* peripherals base addresses */
+
+
+#include "src.h"
+#include "dst.h"
+
+#define __DEBUG__
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
+#define SHORT_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c"
+#define SHORT_TO_BINARY(byte)  \
+  (byte & 0x8000 ? '1' : '0'), \
+  (byte & 0x4000 ? '1' : '0'), \
+  (byte & 0x2000 ? '1' : '0'), \
+  (byte & 0x1000 ? '1' : '0'), \
+  (byte & 0x800 ? '1' : '0'), \
+  (byte & 0x400 ? '1' : '0'), \
+  (byte & 0x200 ? '1' : '0'), \
+  (byte & 0x100 ? '1' : '0'), \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
+
+#define MAX_PRINT_ELEMENTS (4)
+
+const unsigned INPUT_N_ELEMENTS = src_SAMPLE_COUNT*src_FEATURE_COUNT;
+const unsigned OUTPUT_N_ELEMENTS = dst_SAMPLE_COUNT*dst_FEATURE_COUNT;
+
+#if 0
+/* base address for the accelerator */
+#define MEM_BASE_ADDR XPAR_PS7_DDR_0_S_AXI_BASEADDR
+
+/* data offsets and pointers */
+#define SRC_BUFFER_BASE (MEM_BASE_ADDR + 0x00000000)
+unsigned short *src_mem = (unsigned short*)SRC_BUFFER_BASE;
+
+#define GLD_BUFFER_BASE (MEM_BASE_ADDR + 0x00010000)
+unsigned short *gld_mem = (unsigned short*)GLD_BUFFER_BASE;
+
+#define DST_BUFFER_BASE (MEM_BASE_ADDR + 0x00020000)
+unsigned short *dst_mem = (unsigned short*)DST_BUFFER_BASE;
+#else
+unsigned short *src_mem;
+unsigned short *gld_mem;
+unsigned short *dst_mem;
+#endif
+
+/* accelerator configuration */
+XMyproject do_jet_tagger;
+XMyproject_Config *do_jet_tagger_cfg;
+
+/* accelerator initialization routine */
+void init_accelerators()
+{
+    printf("INFO: Initializing accelerator\n\r");
+    do_jet_tagger_cfg = XMyproject_LookupConfig(XPAR_MYPROJECT_0_DEVICE_ID);
+    if (do_jet_tagger_cfg)
+    {
+        int status  = XMyproject_CfgInitialize(&do_jet_tagger, do_jet_tagger_cfg);
+        if (status != XST_SUCCESS)
+        {
+            printf("ERROR: Initializing accelerator\n\r");
+        }
+    }
+}
+
+//#if defined(__HPC_ACCELERATOR__) || defined(__ACP_ACCELERATOR__)
+///*
+// *  TODO: remember to edit core_baremetal_polling_bsp/psu_cortexa53_0/libsrc/standalon_v6_5/src/bspconfig.h
+// *
+// *  #define EL1_NONSECURE 1
+// *
+// */
+//void init_accelerator_coherency(UINTPTR base_addr)
+//{
+//    /* Enable snooping of APU caches from CCI */
+//    Xil_Out32(0xFD6E4000, 0x1);
+//
+//    /* Configure AxCACHE for write-back read and write-allocate (ARCACHE is [7:4], AWCACHE is [11:8]) */
+//    /* Configure AxPROT[2:0] for data access [2], secure access [1], unprivileged access [0] */
+//    Xil_Out32(base_addr, 0xFF0);
+//}
+//#endif
+
+/* golden model of the accelerator in software */
+int jet_tagger_sw(unsigned short *src, unsigned short *dst, unsigned input_n_elements, unsigned output_n_elements)
+{
+    printf("INFO: Golden results are pre-compiled. It would be nice to run a software model here.\n");
+    // See src.h and dst.h for input and golden output respectively.
+    return 0;
+}
+
+/* profiling function */
+double get_elapsed_time(XTime start, XTime stop)
+{
+    return 1.0 * (stop - start) / (COUNTS_PER_SECOND);
+}
+
+/* dump data to the console */
+void dump_data(const char* label, unsigned short* data, unsigned sample_count, unsigned feature_count, unsigned print_hex, unsigned print_bin)
+{
+    printf("INFO:   %s[%u][%u]:\n\r", label, sample_count, feature_count);
+    /* print at most MAX_PRINT_ELEMENTS */
+    for (unsigned i = 0; i < sample_count && i < MAX_PRINT_ELEMENTS; i++)
+    {
+    	printf("INFO:     [%u] ", i);
+    	if (print_hex)
+    		for (unsigned j = 0; j < feature_count; j++)
+    		{
+    			unsigned index = i * feature_count + j;
+    			printf("%03X ", data[index]);
+    		}
+    	if (print_bin)
+    		for (unsigned j = 0; j < feature_count; j++)
+    		{
+    			unsigned index = i * feature_count + j;
+    			printf(""SHORT_TO_BINARY_PATTERN, SHORT_TO_BINARY(data[index]));
+    			printf(" ");
+    		}
+    	printf("\n\r");
+    }
+    for (unsigned i = sample_count - MAX_PRINT_ELEMENTS; i < sample_count; i++)
+    {
+    	printf("INFO:     [%u] ", i);
+    	if (print_hex)
+    		for (unsigned j = 0; j < feature_count; j++)
+    		{
+    			unsigned index = i * feature_count + j;
+    			printf("%03X ", data[index]);
+    		}
+    	if (print_bin)
+    		for (unsigned j = 0; j < feature_count; j++)
+    		{
+    			unsigned index = i * feature_count + j;
+    			printf(""SHORT_TO_BINARY_PATTERN, SHORT_TO_BINARY(data[index]));
+    			printf(" ");
+    		}
+    	printf("\n\r");
+    }
+}
+
+/* the top of the hill :-) */
+int main(int argc, char** argv)
+{
+
+    XTime start, stop;
+    double calibration_time;
+    double sw_elapsed;
+    char __attribute__ ((unused)) dummy; /* dummy input */
+
+    int hw_errors;
+    double hw_elapsed = 0;
+    double cache_elapsed = 0;
+
+    printf("\n\r");
+    printf("INFO: ===============================================\n\r");
+    printf("INFO: Jet Tagger (w/ polling)\n\r");
+    printf("INFO: ===============================================\n\r");
+
+    /* initialize platform (uart and caches) */
+    init_platform();
+
+    init_accelerators();
+
+    src_mem = malloc(INPUT_N_ELEMENTS * sizeof(unsigned short));
+    dst_mem = malloc(OUTPUT_N_ELEMENTS * sizeof(unsigned short));
+    gld_mem = malloc(OUTPUT_N_ELEMENTS * sizeof(unsigned short));
+
+    /* calibration */
+    XTime_GetTime(&start);
+    sleep(1);
+    XTime_GetTime(&stop);
+    calibration_time = get_elapsed_time(start, stop);
+    printf("INFO: Time calibration for one second (%lf sec)\n\r", calibration_time);
+
+    /* initialize memory */
+    printf("INFO: Initialize memory\n\r");
+    printf("INFO:   - Sample count: %u\n\r", src_SAMPLE_COUNT); /* Same as dst_SAMPLE_COUNT */
+    printf("INFO:   - Input-feature count: %u\n\r", src_FEATURE_COUNT);
+    printf("INFO:   - Output-class count: %u\n\r", dst_FEATURE_COUNT);
+    printf("INFO:   - Data size: %u B\n\r", sizeof(unsigned short));
+    printf("INFO:   - Total input size: %u B, %.2f KB, %.2f MB\n\r", src_FEATURE_COUNT * src_SAMPLE_COUNT * sizeof(unsigned short), (src_FEATURE_COUNT * src_SAMPLE_COUNT * sizeof(unsigned short)) / (float)1024, (src_FEATURE_COUNT * src_SAMPLE_COUNT * sizeof(unsigned short)) / (float)(1024*1024));
+    printf("INFO:   - Total output size: %u B, %.2f KB, %.2f MB\n\r", dst_FEATURE_COUNT * dst_SAMPLE_COUNT * sizeof(unsigned short), (dst_FEATURE_COUNT * dst_SAMPLE_COUNT * sizeof(unsigned short)) / (float)1024, (dst_FEATURE_COUNT * dst_SAMPLE_COUNT * sizeof(unsigned short)) / (float)(1024*1024));
+
+    // Set Heap Size in ldscript.ld to 0x1000000 (16MB)
+    //malloc_stats();
+
+    for (int i = 0; i < INPUT_N_ELEMENTS; i++) {
+        src_mem[i] = src_data[i];
+    }
+    for (int i = 0; i < OUTPUT_N_ELEMENTS; i++) {
+        gld_mem[i] = dst_data[i];
+        dst_mem[i] = 0x0;
+    }
+
+    /* ****** SOFTWARE REFERENCE ****** */
+#ifdef __DEBUG__
+    printf("INFO: Start SW accelerator\n\r");
+#endif
+    XTime_GetTime(&start);
+
+    jet_tagger_sw(src_mem, gld_mem, INPUT_N_ELEMENTS, OUTPUT_N_ELEMENTS);
+    XTime_GetTime(&stop);
+    sw_elapsed = get_elapsed_time(start, stop);
+
+    /* ****** ACCELERATOR ****** */
+    xil_printf("INFO: Press any key to start the accelerator: ");
+    dummy = inbyte();
+    printf("\n\rINFO: \n\r");
+
+    XTime_GetTime(&start);
+    Xil_DCacheFlushRange((UINTPTR)src_mem, INPUT_N_ELEMENTS * sizeof(unsigned short));
+    Xil_DCacheFlushRange((UINTPTR)dst_mem, OUTPUT_N_ELEMENTS * sizeof(unsigned short));
+    Xil_DCacheFlushRange((UINTPTR)gld_mem, OUTPUT_N_ELEMENTS * sizeof(unsigned short));
+    XTime_GetTime(&stop);
+    cache_elapsed = get_elapsed_time(start, stop);
+
+#ifdef __DEBUG__
+    printf("INFO: Configure and start accelerator\n\r");
+#endif
+
+    unsigned short *src_mem_i = src_mem;
+    unsigned short *dst_mem_i = dst_mem;
+
+    for (unsigned i = 0; i < src_SAMPLE_COUNT; i++) {
+
+    	/* Configure the accelerator */
+    	XTime_GetTime(&start);
+    	XMyproject_Set_src_V(&do_jet_tagger, (unsigned)src_mem_i);
+    	XMyproject_Set_dst_V(&do_jet_tagger, (unsigned)dst_mem_i);
+
+    	XMyproject_Start(&do_jet_tagger);
+
+    	/* polling */
+    	while (!XMyproject_IsDone(&do_jet_tagger));
+
+    	/* get error status */
+    	//hw_flags = XMyproject_Get_return(&do_jet_tagger);
+    	XTime_GetTime(&stop);
+    	hw_elapsed += get_elapsed_time(start, stop);
+
+    	src_mem_i += src_FEATURE_COUNT;
+    	dst_mem_i += dst_FEATURE_COUNT;
+    }
+
+	XTime_GetTime(&start);
+	Xil_DCacheFlushRange((UINTPTR)dst_mem, OUTPUT_N_ELEMENTS * sizeof(unsigned short));
+	XTime_GetTime(&stop);
+	cache_elapsed += get_elapsed_time(start, stop);
+
+    /* ****** VALIDATION ****** */
+
+#ifdef __DEBUG__
+    printf("INFO: ================== Validation =================\n\r");
+    printf("INFO: Dump data\n\r");
+    dump_data("src", (unsigned short*)src_mem, src_SAMPLE_COUNT, src_FEATURE_COUNT, 1, 0);
+    dump_data("sw_dst", (unsigned short*)gld_mem, dst_SAMPLE_COUNT, dst_FEATURE_COUNT, 1, 0);
+    dump_data("hw_dst", (unsigned short*)dst_mem, dst_SAMPLE_COUNT, dst_FEATURE_COUNT, 1, 0);
+#endif
+
+    printf("INFO: Software execution time: %f sec\n\r", sw_elapsed);
+
+    printf("INFO: Accelerator execution time: %f sec\n\r", hw_elapsed);
+    printf("INFO: Cache flush time: %f sec\n\r", cache_elapsed);
+    printf("INFO: Accelerator/software speedup (the sofware is fake so this does not count...): %.2f X\n\r", (sw_elapsed >= (hw_elapsed+cache_elapsed))?(sw_elapsed/(hw_elapsed+cache_elapsed)):-((hw_elapsed+cache_elapsed)/sw_elapsed));
+
+    /* Accelerator validation */
+    hw_errors = 0;
+    for (int i = 0; i < OUTPUT_N_ELEMENTS; i++)
+    {
+        if (dst_mem[i] != gld_mem[i])
+        {
+            printf("ERROR: [%d]: Accelerator hw %03X != sw %03X\n\r", i, dst_mem[i], gld_mem[i]);
+            hw_errors++;
+        }
+    }
+    printf("INFO: Total errors = %d (out of %d elements)\n\r", hw_errors, OUTPUT_N_ELEMENTS);
+    if (hw_errors > 0)
+        printf("INFO: Accelerator validation: FAIL\n\r");
+    else
+        printf("INFO: Accelerator validation: PASS!\n\r");
+
+    printf("INFO: done!\n\r");
+    printf("INFO: ===============================================\n\r");
+
+    cleanup_platform();
+
+    return 0;
+}
+
+
