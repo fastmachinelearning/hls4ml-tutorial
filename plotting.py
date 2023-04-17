691125af8ec5183342ec408bfd5df877f64d5ba7
Javier Duarte <jduarte@ucsd.edu>
2023-04-16 17:34:19 -0700

    pre-commit

--- plotting.py
+++ plotting.py
@@ -1,16 +1,14 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
-import pandas as pd
-from sklearn.metrics import confusion_matrix
 import itertools
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import auc, roc_curve
+
 
 # confusion matrix code from Maurizio
 # /eos/user/m/mpierini/DeepLearning/ML4FPGA/jupyter/HbbTagger_Conv1D.ipynb
-def plot_confusion_matrix(cm, classes,
-                          normalize=False, 
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
@@ -19,35 +17,41 @@ def plot_confusion_matrix(cm, classes,
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    #plt.title(title)
+    # plt.title(title)
     cbar = plt.colorbar()
-    plt.clim(0,1)
+    plt.clim(0, 1)
     cbar.set_label(title)
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
     fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
+    thresh = cm.max() / 2.0
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
 
-    #plt.tight_layout()
+    # plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
+
 def plotRoc(fpr, tpr, auc, labels, linestyle, legend=True):
-    for i, label in enumerate(labels):
-        plt.plot(tpr[label],fpr[label],label='%s tagger, AUC = %.1f%%'%(label.replace('j_',''),auc[label]*100.),linestyle=linestyle)
+    for _i, label in enumerate(labels):
+        plt.plot(
+            tpr[label],
+            fpr[label],
+            label='{} tagger, AUC = {:.1f}%'.format(label.replace('j_', ''), auc[label] * 100.0),
+            linestyle=linestyle,
+        )
     plt.semilogy()
     plt.xlabel("Signal Efficiency")
     plt.ylabel("Background Efficiency")
-    plt.ylim(0.001,1)
+    plt.ylim(0.001, 1)
     plt.grid(True)
-    if legend: plt.legend(loc='upper left')
-    plt.figtext(0.25, 0.90,'hls4ml',fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+    if legend:
+        plt.legend(loc='upper left')
+    plt.figtext(0.25, 0.90, 'hls4ml', fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+
 
 def rocData(y, predict_test, labels):
 
@@ -58,28 +62,30 @@ def rocData(y, predict_test, labels):
     auc1 = {}
 
     for i, label in enumerate(labels):
-        df[label] = y[:,i]
-        df[label + '_pred'] = predict_test[:,i]
+        df[label] = y[:, i]
+        df[label + '_pred'] = predict_test[:, i]
 
-        fpr[label], tpr[label], threshold = roc_curve(df[label],df[label+'_pred'])
+        fpr[label], tpr[label], threshold = roc_curve(df[label], df[label + '_pred'])
 
         auc1[label] = auc(fpr[label], tpr[label])
     return fpr, tpr, auc1
 
+
 def makeRoc(y, predict_test, labels, linestyle='-', legend=True):
 
-    if 'j_index' in labels: labels.remove('j_index')
-        
+    if 'j_index' in labels:
+        labels.remove('j_index')
+
     fpr, tpr, auc1 = rocData(y, predict_test, labels)
     plotRoc(fpr, tpr, auc1, labels, linestyle, legend=legend)
     return predict_test
 
+
 def print_dict(d, indent=0):
-    align=20
     for key, value in d.items():
         print('  ' * indent + str(key), end='')
         if isinstance(value, dict):
             print()
-            print_dict(value, indent+1)
+            print_dict(value, indent + 1)
         else:
             print(':' + ' ' * (20 - len(key) - 2 * indent) + str(value))
